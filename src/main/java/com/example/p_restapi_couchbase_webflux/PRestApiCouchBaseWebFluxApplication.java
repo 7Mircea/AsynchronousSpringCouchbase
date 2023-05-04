c68f2ffe310597e5ff88bc924457ee06056b527c
Gheoace Mircea <7mircea.c@gmail.com>
2023-05-04 05:38:28 +0300

    works with first N products from jmeter

---
+++ src/main/java/com/example/p_restapi_couchbase_webflux/PRestApiCouchBaseWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.p_restapi_couchbase_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PRestApiCouchBaseWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PRestApiCouchBaseWebFluxApplication.class, args);
+    }
+
+}
